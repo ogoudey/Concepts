de5023ccdb44b65ab9255a9cc4e5cee961c869e6
Olin <olin.goog@gmail.com>
2026-09-11 13:20:31 -0400

    removed circle import

--- concepts/capability_model.py
+++ concepts/capability_model.py
@@ -88,6 +88,7 @@ class CapabilityModel(BaseModel):
 # ------------ Exampels ---------------- #
 kinova_vla_capability_model = \
 CapabilityModel(
+    id="kinova_vla"
     inputs=[
         RootIntegratedInput(
             description="the perceptual inputs from cameras and kinova and the governing language"
