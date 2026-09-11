43ec2b0d9ec0bab0839e9b020a7bfb34b0cfd216
Olin <olin.goog@gmail.com>
2026-09-11 09:47:28 -0400

    merged

--- concepts/ego.py
+++ concepts/ego.py
@@ -1,4 +1,4 @@
-from typing import Dict, Iterator, Optional, Union, override
+from typing import Dict, Iterator, Optional, Union, override, List
 from dataclasses import dataclass, Field
 from .capability_model import Action
 from pydantic import BaseModel, Field, field_serializer, field_validator
@@ -6,9 +6,11 @@ from .experience_dna import ExperienceDNA
 
 class Ego(BaseModel):
     codebase: Optional[str]
+    capability_ids: List[str]
     experience_dna: ExperienceDNA
 
 class HumanEgo(BaseModel):
     codebase: Optional[str]
+    capability_ids: List[str]
     experience_dna: ExperienceDNA
     name: str
